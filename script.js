d6f12f8b8bf802bcb94779f8a6835d349a1aa639
alexander-CD21 <alexander.guillen.v@uni.pe>
2022-01-10 16:59:51 -0500

    Practica Terminada

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("page loaded...");
+/*let vid=document.getElementById("mp4");
+console.log(vid);
+function over(){
+    vid.play();
+    vid.muted=true;
+}
+function out(){
+    vid.pause();
+    vid.muted=true;
+}*/
+
+function over(inicio){
+    inicio.play();
+    inicio.muted=true;
+}
+
+function out(inicio1){
+    inicio1.pause();
+    inicio1.muted=true;
+}
